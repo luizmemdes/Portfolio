bfe47296abd93d77c6c8e72846b0c29a62e92df6
Luiz Antônio Mendes Nasário <luiznmendes2@gmail.com>
2024-11-12 18:15:30 -0300

    ajuste json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
+  
