6ac32ec2fcbee649c79813042b3bc9f721d1cb60
Luiz Antônio Mendes Nasário <luiznmendes2@gmail.com>
2024-11-14 13:57:06 -0300

    mudanca no deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    }
+  output: 'export',
+   basePath: '/Portfolio',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
   }
-  
-  module.exports = nextConfig
-  
+};
+
+module.exports = nextConfig;
+
